a590c57bfaf43f7af43382904c829de0dab674a3
Miguel Castillo <miguelcastillo@MBP-de-Miguel.fritz.box>
2021-07-15 13:06:48 -0600

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ const forecast = require('./utils/forecast.js')
 
 
 const app = express()
+const port = process.env.PORT || 3000
+
+
 
 //Define paths for Express config 
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -80,6 +83,6 @@ app.get('*',(req, res)=>{
             errorMessage: 'Page not found'})
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port '+port)
 })
